828bbc4253e1c7d6c247f014b27da835b5c09ca2
ShelDenis <shele1-2010@mail.ru>
2024-12-11 16:23:10 +0600

    chore: angle improved and tests added

--- SpaceBattle.Lib/Angle.cs
+++ SpaceBattle.Lib/Angle.cs
@@ -1,17 +1,17 @@
-public class Angle : IConvertible
+public class Angle
 {
     int numerator;
     int denominator;
 
     public Angle(int n, int d)
     {
-        numerator = n;
+        numerator = n % d;
         denominator = d;
     }
 
     public int Numerator
     {
-        get => numerator;
+        get => numerator % denominator;
     }
 
     public int Denominator
@@ -26,109 +26,37 @@ public class Angle : IConvertible
         return new Angle(new_num, a1.Denominator);
     }
 
-    public double ToDouble(IFormatProvider? provider)
+    public double Sin()
     {
-        return (double)numerator / denominator * Math.PI / 180;
+        double n = numerator;
+        double d = denominator;
+        return Math.Sin(n / d * Math.PI * 2);
+    }
+
+    public double Cos()
+    {
+        double n = numerator;
+        double d = denominator;
+        return Math.Cos(n / d * Math.PI * 2);
     }
 
     public static bool operator ==(Angle a1, Angle a2)
     {
-        return a1.numerator == a2.numerator;
+        return a1.GetHashCode() == a2.GetHashCode();
     }
 
     public static bool operator !=(Angle a1, Angle a2)
     {
-        return a1.numerator != a2.numerator;
+        return a1.GetHashCode() != a2.GetHashCode();
     }
 
     public override bool Equals(object? obj)
     {
-        return obj != null && obj is Angle angle && Numerator == angle.Numerator;
+        return obj != null && obj is Angle angle && (Numerator % Denominator) == (angle.Numerator % angle.Denominator);
     }
 
     public override int GetHashCode()
     {
-        return numerator.GetHashCode();
+        return (numerator % denominator).GetHashCode();
     }
-
-    public TypeCode GetTypeCode()
-    {
-        throw new NotImplementedException();
-    }
-
-    public bool ToBoolean(IFormatProvider? provider)
-    {
-        throw new NotImplementedException();
-    }
-
-    public byte ToByte(IFormatProvider? provider)
-    {
-        throw new NotImplementedException();
-    }
-
-    public char ToChar(IFormatProvider? provider)
-    {
-        throw new NotImplementedException();
-    }
-
-    public DateTime ToDateTime(IFormatProvider? provider)
-    {
-        throw new NotImplementedException();
-    }
-
-    public decimal ToDecimal(IFormatProvider? provider)
-    {
-        throw new NotImplementedException();
-    }
-
-    public short ToInt16(IFormatProvider? provider)
-    {
-        throw new NotImplementedException();
-    }
-
-    public int ToInt32(IFormatProvider? provider)
-    {
-        throw new NotImplementedException();
-    }
-
-    public long ToInt64(IFormatProvider? provider)
-    {
-        throw new NotImplementedException();
-    }
-
-    public sbyte ToSByte(IFormatProvider? provider)
-    {
-        throw new NotImplementedException();
-    }
-
-    public float ToSingle(IFormatProvider? provider)
-    {
-        throw new NotImplementedException();
-    }
-
-    public string ToString(IFormatProvider? provider)
-    {
-        throw new NotImplementedException();
-    }
-
-    public object ToType(Type conversionType, IFormatProvider? provider)
-    {
-        throw new NotImplementedException();
-    }
-
-    public ushort ToUInt16(IFormatProvider? provider)
-    {
-        throw new NotImplementedException();
-    }
-
-    public uint ToUInt32(IFormatProvider? provider)
-    {
-        throw new NotImplementedException();
-    }
-
-    public ulong ToUInt64(IFormatProvider? provider)
-    {
-        throw new NotImplementedException();
-    }
-
 }
