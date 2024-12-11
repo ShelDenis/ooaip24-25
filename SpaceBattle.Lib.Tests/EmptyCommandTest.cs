7d069d4051eee6f0f41918a8ace37eb1d4feca6b
ShelDenis <shele1-2010@mail.ru>
2024-12-11 20:05:20 +0600

    feat: empty command

---
+++ SpaceBattle.Lib.Tests/EmptyCommandTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using Moq;
+namespace SpaceBattle.Lib.Tests;
+
+public class EmptyCommandTest
+{
+    [Fact]
+    public void Execute_EmptyCommand()
+    {
+        var empty_cmd = new Mock<ICommand>();
+        empty_cmd.Object.Execute();
+
+        empty_cmd.Verify(x => x.Execute());
+    }
+}
