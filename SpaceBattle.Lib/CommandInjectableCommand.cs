46a7a28b33254287e3312fad356c457d28322e13
ShelDenis <shele1-2010@mail.ru>
2024-12-11 20:14:05 +0600

    chore: tests for inject command

---
+++ SpaceBattle.Lib/CommandInjectableCommand.cs
@@ -0,0 +1,16 @@
+namespace SpaceBattle.Lib;
+
+public class InjectableCommand : ICommand, ICommandInjectable
+{
+    private ICommand _cmd = new EmptyCommand();
+    public void Execute()
+    {
+        if (_cmd.GetType() == typeof(EmptyCommand))
+            throw new Exception("There is nothing to execute!");
+        _cmd.Execute();
+    }
+    public void Inject(ICommand cmd)
+    {
+        _cmd = cmd;
+    }
+}
