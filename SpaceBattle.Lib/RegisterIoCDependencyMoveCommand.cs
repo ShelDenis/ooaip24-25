10dfbf05ad9272244b3282e2a298fc19e5e205f2
ShelDenis <shele1-2010@mail.ru>
2024-12-12 17:14:47 +0600

    feat: register ioc move command

--- SpaceBattle.Lib/RegisterIoCDependencyMoveCommand.cs
+++ SpaceBattle.Lib/RegisterIoCDependencyMoveCommand.cs
@@ -1,1 +1,12 @@
-//заглушка
+using Hwdtech;
+namespace SpaceBattle.Lib;
+
+public class RegisterIoCDependencyMoveCommand : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<ICommand>("IoC.Register", 
+                            "Commands.Move",
+        (object[] args) => {return new MoveCommand(IoC.Resolve<IMoving>("Adapters.IMovingObject"));}).Execute();
+    }
+}
