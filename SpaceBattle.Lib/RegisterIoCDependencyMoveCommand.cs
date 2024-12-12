3783aa58a30c93630531a5b2e64ef55cb20a4a95
ShelDenis <shele1-2010@mail.ru>
2024-12-12 17:17:54 +0600

    chore: register move

--- SpaceBattle.Lib/RegisterIoCDependencyMoveCommand.cs
+++ SpaceBattle.Lib/RegisterIoCDependencyMoveCommand.cs
@@ -5,8 +5,8 @@ public class RegisterIoCDependencyMoveCommand : ICommand
 {
     public void Execute()
     {
-        IoC.Resolve<ICommand>("IoC.Register", 
+        IoC.Resolve<ICommand>("IoC.Register",
                             "Commands.Move",
-        (object[] args) => {return new MoveCommand(IoC.Resolve<IMoving>("Adapters.IMovingObject"));}).Execute();
+        (object[] args) => { return new MoveCommand(IoC.Resolve<IMoving>("Adapters.IMovingObject")); }).Execute();
     }
 }
