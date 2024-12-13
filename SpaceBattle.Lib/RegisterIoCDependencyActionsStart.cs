2c6c29c97fc15c3a82f3f579c0742d71fa2724d4
ShelDenis <shele1-2010@mail.ru>
2024-12-14 00:42:07 +0600

    chore: work starts about start

---
+++ SpaceBattle.Lib/RegisterIoCDependencyActionsStart.cs
@@ -0,0 +1,10 @@
+using App;
+namespace SpaceBattle.Lib;
+
+public class RegisterIoCDependencyActionsStart: ICommand
+{
+    public void Execute()
+    {
+        
+    }
+}
---
+++ SpaceBattle.Lib/StartCommand.cs
@@ -0,0 +1,15 @@
+using System.Security.Cryptography.X509Certificates;
+using App;
+
+namespace SpaceBattle.Lib;
+
+public class StartCommand: ICommand
+{
+    public StartCommand(string name, object[] args, int id)
+    {}
+
+    public void Execute()
+    {
+        
+    }
+}
