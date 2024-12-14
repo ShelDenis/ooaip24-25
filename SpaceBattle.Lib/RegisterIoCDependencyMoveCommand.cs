940ffe29522a7c13b6e52fd9c94c1382ac98abec
Mawak0 <a32221580@gmail.com>
2024-12-14 16:25:05 +0600

    feat: RegisterIoCDependencyMoveCommand added

--- SpaceBattle.Lib/RegisterIoCDependencyMoveCommand.cs
+++ SpaceBattle.Lib/RegisterIoCDependencyMoveCommand.cs
@@ -1,12 +1,15 @@
-using Hwdtech;
+
+using App;
 namespace SpaceBattle.Lib;
 
 public class RegisterIoCDependencyMoveCommand : ICommand
 {
+
     public void Execute()
     {
-        IoC.Resolve<ICommand>("IoC.Register",
+        Ioc.Resolve<App.ICommand>("IoC.Register",
                             "Commands.Move",
-        (object[] args) => { return new MoveCommand(IoC.Resolve<IMoving>("Adapters.IMovingObject")); }).Execute();
+        (object[] args) => new MoveCommand(Ioc.Resolve<IMoving>("Adapters.IMovingObject", args))).Execute();
+
     }
 }
