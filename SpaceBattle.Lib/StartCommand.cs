c12ca4db3119518e0bd4b96cd1ebca0b00178da1
ShelDenis <shele1-2010@mail.ru>
2024-12-15 19:23:39 +0600

    chore: startcommand scatch

--- SpaceBattle.Lib/StartCommand.cs
+++ SpaceBattle.Lib/StartCommand.cs
@@ -1,15 +1,28 @@
-using System.Security.Cryptography.X509Certificates;
 using App;
 
 namespace SpaceBattle.Lib;
 
 public class StartCommand: ICommand
 {
-    public StartCommand(string name, object[] args, int id)
-    {}
+    string name;
+    object[] args;
+    string key;
+    public StartCommand(string name, object[] args, string key)
+    {
+        this.name = name;
+        this.args = args;
+        this.key = key;
+    }
 
     public void Execute()
     {
-        
+        var mc = Ioc.Resolve<ICommand>("Macro." + name, args);
+        var inj = Ioc.Resolve<ICommandInjectable>("Commands.CommandInjectable");
+        inj.Inject(mc);
+        var q = Ioc.Resolve<ICommandReceiver>("Game.Queue");
+        var sendcmd = Ioc.Resolve<ICommand>("Commands.Send", q, inj);
+        sendcmd.Execute();
+        var objs = Ioc.Resolve<IDictionary<string, IDictionary<string, object>>>("Game.Objects");
+        objs[key].Add(name, inj);
     }
 }
